59368aa16a2a10303be22679e4225c9e4bd464cf
eric-mendoza <men15002@uvg.edu.gt>
2019-02-18 23:49:20 -0600

    Commit inicial

---
+++ AStar.py
@@ -0,0 +1,7 @@
+#  Universidad del Valle de Guatemala
+#  Inteligencia Artificial
+#  Eric Mendoza: 15002
+#  Proyecto 1: Agente inteligente
+#  AStar.py: El presente archivo ejecuta una version generica de A*
+
+print("Hola Mundo")
